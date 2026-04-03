aaf3e36b7ff26496b6d12530aa97d2a7ac9d51f2
yemiiz <415595323@qq.com>
2026-04-03 18:25:13 +0800

    链表加法 第二版 采用每一位超过十进位

--- object-oriented programming(C++)/20260402_nodes/add_nodes.cpp
+++ object-oriented programming(C++)/20260402_nodes/add_nodes.cpp
@@ -53,41 +53,60 @@ int main()
 
 
 //函数实现
+// 反转链表的函数
+ListNode *reverse(ListNode *head)
+{
+    ListNode *pre = NULL;
+    ListNode *cur = head;
+    while (cur != NULL)
+    {
+        ListNode *next = cur->next;
+        cur->next = pre;
+        pre = cur;
+        cur = next;
+    }
+    return pre;
+}
+
 ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
 {
-    // 这个函数需要你补充完整
-    ListNode *p1 = l1, *p2 = l2, *head1 = NULL;
-    int num1 = 0;
-    int num2 = 0;
-    while (p1 != NULL)
+    l1 = reverse(l1);
+    l2 = reverse(l2);
+    ListNode *p1 = l1, *p2 = l2, *head = NULL, *tail = NULL;
+    int c = 0;
+    while (p1 != NULL || p2 != NULL || c != 0)
     {
-        num1 *= 10;
-        num1 += p1->val;
-        p1 = p1->next;
-    }
-    while (p2 != NULL)
-    {
-        num2 *= 10;
-        num2 += p2->val;
-        p2 = p2->next;
-    }
-    int sum = num1 + num2;
-    while (sum)
-    {
-        ListNode *n1 = new ListNode();
-        int temp = sum % 10;
-        n1->val = temp;
-        if (head1 == NULL)
+        int a = 0;
+        if (p1 != NULL)
         {
-            head1 = n1;
+            a = p1->val;
+            p1 = p1->next;
+        }
+        int b = 0;
+        if (p2 != NULL)
+        {
+            b = p2->val;
+            p2 = p2->next;
+        }
+        int s = a + b + c;
+        c = s / 10;
+        int cur = s % 10;
+
+        ListNode *node = new ListNode();
+        node->val = cur;
+        node->next = NULL;
+
+        if (head == NULL)
+        {
+            head = node;
+            tail = node;
         }
         else
         {
-            n1->next = head1;
-            head1 = n1;
+            tail->next = node;
+            tail = node;
         }
-        sum /= 10;
     }
-
-    return head1;
+    head = reverse(head);
+    return head;
 }
